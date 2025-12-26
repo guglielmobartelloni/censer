6effb447f25fb54f90e780b24bc6c13bb45f0ccf
Guglielmo <samurai@MacBook-Air-di-Guglielmo.local>
2025-12-27 00:33:06 +0100

    chore: new version and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 0.2.0
+
+- Added support for graphql fragments with cartesian product combinations
+- Added simple error handling
+
+# 0.1.0
+
+- First working iteration
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Censer.MixProject do
   def project do
     [
       app: :censer,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       description: "Vaporize GraphQL queries into Elixir pattern matches.",
       start_permanent: Mix.env() == :prod,
